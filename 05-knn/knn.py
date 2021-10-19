972989743ebc9be72d0e1063523deb3f768023dd
Jesus Omar <jesomar.cuenca@gmail.com>
2021-10-18 22:14:59 -0500

    KNN Implementation

--- 05-knn/knn.py
+++ 05-knn/knn.py
@@ -9,6 +9,7 @@ Date: 18/10/2021
 """
 
 import numpy as np
+from collections import Counter
 
 
 class Knn:
@@ -23,23 +24,19 @@ class Knn:
     def euclidean_distance(self, example1, example2):
         # TODO: Implement the euclidean distance function to calculate the distance between the provided elements
         # The output should be a scalar
-        temp = example1 - example2
-        distance = temp @ temp.T
-        return np.sqrt(distance)
+        return np.sqrt(np.sum((example2 - example1)**2))
 
     def get_neighbors(self, example):
         # TODO: Implement this function to obtain the k nearest neighbours for the provided example
         # You must return an np array of shape 1 x k, each element must be the index of the neighbour so you can associate it with its class/label later.
         # You will need to calculate  the distance and then sort, recalling the indexes. Do your research to find a method that allows this.
         # Remember thatin self.model_x you have your list of examples
-        distances = []
-
-        for i in self.model_x:
-            distances.append(self.euclidean_distance(i, example))
+        distances = [ self.euclidean_distance(i, example) for i in self.model_x ]
         
-        neighbor_indices = np.array(distances).argsort()[:self.k]
+        neighbor_indices    = np.argsort(distances)[:self.k]
+        neighbor_labels     = [ self.model_y[i][0] for i in neighbor_indices ]
 
-        return neighbor_indices
+        return Counter(neighbor_labels).most_common(1)[0][0]
 
     def fit(self, X, y):
         """
@@ -47,8 +44,8 @@ class Knn:
         X is an np array of n x m shape, n is the number of features, m is the number of examples
         y is an np array of 1 x m shape, with the corresponding class for each example in X
         """
-        self.model_x = X
-        self.model_y = y
+        self.model_x = X.T
+        self.model_y = y.T
 
     def predict(self, example):
         # TODO: Implement this function to predict the class for the provided example.
@@ -58,4 +55,4 @@ class Knn:
         # Do your research to find numpy or python functions that allow to sort and count
         # In self.model_y you will have the list of the classes for examples in original dataset (self.model_x). self.model_y is of shape 1xm
         temp = self.get_neighbors(example)
-        return temp
+        return np.array(temp)
